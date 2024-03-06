5050681607b58f4abc3ce5e0852c3710bfe2c755
Nikolai Nikolaev <niki_nik_74@abv.bg>
2024-03-06 15:06:39 +0200

    Create module export

---
+++ api/config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    development: {
+        PORT: 5000,
+        dbAtlas: 
+        "mongodb+srv://nikindtmas1:niki548444@cluster0.jgtvday.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    },
+
+    productio: {
+        PORT: 80
+    }
+};
