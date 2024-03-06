aae9b6fd319d836f116b51c35fe6e1d6737b5ce5
Nikolai Nikolaev <niki_nik_74@abv.bg>
2024-03-06 14:36:55 +0200

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,19 @@
+let Express = require("express");
+let MongoClient = require('mongodb').MongoClient;
+let cors = require('cors');
+const multer = require("multer");
+
+let app = Express();
+app.use(cors());
+
+let CONNECTION_STRING="mongodb+srv://nikindtmas1:<niki548444>@cluster0.jgtvday.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+let DATABASENAME = "todoappdb";
+let database;
+
+app.listen(5038, () => {
+    MongoClient.connect(CONNECTION_STRING,(error, client)=>{
+        database=client.db(DATABASENAME);
+        console.log("Mongodb Connection successful");
+    })
+})
