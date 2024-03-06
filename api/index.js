8732c1d595cebe91d350c0d8f6e1cb5ccc45b37f
Nikolai Nikolaev <niki_nik_74@abv.bg>
2024-03-06 14:58:53 +0200

    Install mongoose

--- api/index.js
+++ api/index.js
@@ -1,25 +1,27 @@
-let Express = require("express");
-let MongoClient = require('mongodb').MongoClient;
-let cors = require('cors');
-const multer = require("multer");
+// let Express = require("express");
+// let MongoClient = require('mongodb').MongoClient;
+// let cors = require('cors');
+// const multer = require("multer");
 
-let app = Express();
-app.use(cors());
+// let app = Express();
+// app.use(cors());
 
-let CONNECTION_STRING="mongodb+srv://nikindtmas1:niki548444@cluster0.jgtvday.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// let CONNECTION_STRING="mongodb+srv://nikindtmas1:niki548444@cluster0.jgtvday.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-let DATABASENAME = "todoappdb";
-let database;
+// let DATABASENAME = "todoappdb";
+// let database;
 
-app.listen(5038, () => {
-    MongoClient.connect(CONNECTION_STRING,(error, client)=>{
-        database=client.db(DATABASENAME);
-        console.log("Mongodb Connection successful");
-    })
-});
+// app.listen(5038, () => {
+//     MongoClient.connect(CONNECTION_STRING,(error, client)=>{
+//         database=client.db(DATABASENAME);
+//         console.log("Mongodb Connection successful");
+//     })
+// });
 
-app.get('/api/todoapp/GetNotes', (request, response)=>{
-    database.collection('todoappcollection').find({}).toArray((error, result)=>{
-        response.send(result);
-    })
-})
+// app.get('/api/todoapp/GetNotes', (request, response)=>{
+//     database.collection('todoappcollection').find({}).toArray((error, result)=>{
+//         response.send(result);
+//     })
+// })
+
+const mongoos = require("mongoos");
