01509eb380822a024ab8a132491da9669d7b01c3
Nikolai Nikolaev <niki_nik_74@abv.bg>
2024-03-07 09:18:28 +0200

    Added router get all notes

---
+++ api/controllers/notesController.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const services = require('../services/notesServices');
+
+router.get('/', async (req, res) => {
+    const allNotes = await services.getAllNotes();
+
+    res.json(allNotes);
+});
