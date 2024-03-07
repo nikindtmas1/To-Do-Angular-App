c7db71067415ecba07f5e769dc3f3fe4397dbdd8
Nikolai Nikolaev <niki_nik_74@abv.bg>
2024-03-07 14:03:58 +0200

    Added routers post and get by id

--- api/controllers/notesController.js
+++ api/controllers/notesController.js
@@ -4,9 +4,32 @@ const router = express.Router();
 const services = require('../services/notesServices');
 
 router.get('/', async (req, res) => {
+
     const allNotes = await services.getAllNotes();
 
     res.json(allNotes);
+});
+
+router.get('/:id', async (req, res) => {
+
+    const oneNote = await services.getOneNote(req.params.id);
+
+    res.json(oneNote);
+});
+
+router.post('/', async (req, res) => {
+
+    await services.createNotes({...req.body});
+
+    res.json({ ok : true });
+});
+
+
+router.delete('/:id', async (req, res) => {
+
+    await services.removeNote(req.params.id);
+
+    res.json({ ok: true });
 });
 
 module.exports = router;
