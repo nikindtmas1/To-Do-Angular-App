cb7bb47bf7c89f49c9e5536e9dfd2f2570add5fa
Nikolai Nikolaev <niki_nik_74@abv.bg>
2024-03-07 13:12:51 +0200

    Change connection string

--- api/config/config.js
+++ api/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     development: {
         PORT: 5000,
         dbAtlas: 
-        "mongodb+srv://nikindtmas1:niki548444@cluster0.jgtvday.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+        "mongodb+srv://nikindtmas1:niki548444@cluster0.jgtvday.mongodb.net/todoappdbs?retryWrites=true&w=majority&appName=Cluster0"
     },
 
     productio: {
