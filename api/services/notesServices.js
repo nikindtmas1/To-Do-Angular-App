85debab26a5849b474a4a64b77f22c26ff849dfd
Nikolai Nikolaev <niki_nik_74@abv.bg>
2024-03-07 14:04:35 +0200

    Create services for create, get one and deleet

--- api/services/notesServices.js
+++ api/services/notesServices.js
@@ -1,7 +1,13 @@
 const Notes = require('../models/notesModel');
 
 const getAllNotes = () => Notes.find();
+const createNotes = (data) => Notes.create(data);
+const getOneNote = (id) => Notes.findById(id);
+const removeNote = (id) => Notes.findByIdAndDelete(id);
 
 module.exports = {
     getAllNotes,
+    createNotes,
+    getOneNote,
+    removeNote,
 }
