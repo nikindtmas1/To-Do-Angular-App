154f0f7cd15e79f7773b2cbe1018815c8694f191
Nikolai Nikolaev <niki_nik_74@abv.bg>
2024-03-07 13:26:11 +0200

    Change path

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const notesController = require('../controllers/notesController');
 
-router.use('/todoappcollection', notesController);
+router.use('/notes', notesController);
 
 module.exports = router;
