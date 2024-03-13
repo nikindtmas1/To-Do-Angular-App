2865f7cb2c5f7bb573c80d863646f6f7d3ea7eee
Nikolai Nikolaev <niki_nik_74@abv.bg>
2024-03-13 13:16:43 +0200

    Create dependencis

---
+++ to-do-app/src/app/task-add-edit/task-add-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-add-edit',
+  templateUrl: './task-add-edit.component.html',
+  styleUrl: './task-add-edit.component.css'
+})
+export class TaskAddEditComponent {
+
+}
