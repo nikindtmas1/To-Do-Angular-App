71c502a3e4924f53badcb87321a8ca7afca7b2e7
Nikolai Nikolaev <niki_nik_74@abv.bg>
2024-03-13 15:46:09 +0200

    Added function submit

--- to-do-app/src/app/task-add-edit/task-add-edit.component.ts
+++ to-do-app/src/app/task-add-edit/task-add-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-task-add-edit',
@@ -7,19 +8,36 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrl: './task-add-edit.component.css'
 })
 export class TaskAddEditComponent {
-
+  readonly APIUrl="http://localhost:5000/";
   taskForm:FormGroup
 
-  constructor(private _fb: FormBuilder){
+  constructor(private _fb: FormBuilder, private http:HttpClient){
     this.taskForm = _fb.group({
       description: "description"
     })
   }
 
+  notes:any=[];
+
+  refreshNotes(){
+    this.http.get(this.APIUrl+"notes").subscribe((data)=>{
+      this.notes=data;
+    })
+  };
+
+  ngOnInit(){
+    this.refreshNotes();
+  };
+
   onFormSubmit(){
     if(this.taskForm.value){
-      console.log(this.taskForm.value);
+      const formdata = this.taskForm.value
       
+      console.log(formdata);
+      this.http.post(this.APIUrl+"notes", formdata).subscribe(data => {
+        alert(data);
+        this.refreshNotes();
+      })
     }
   }
 }
