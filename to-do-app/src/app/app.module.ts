ee728aece5c04669eed2795f57c444386fe15536
Nikolai Nikolaev <niki_nik_74@abv.bg>
2024-03-13 11:46:53 +0200

    Added module toolbar and icon

--- to-do-app/src/app/app.module.ts
+++ to-do-app/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,9 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [
     provideAnimationsAsync()
