07ef74dbafa9552ae71bfb9204f49e947f9e8e5c
Nikolai Nikolaev <niki_nik_74@abv.bg>
2024-03-14 10:33:41 +0200

    Create task services ts file

---
+++ to-do-app/src/app/services/task-service.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskServiceService {
+
+  readonly APIUrl="http://localhost:5000/";
+  constructor(private _http:HttpClient) {
+  }
+
+  addedTask(data: any): Observable<any>{
+   return this._http.post(this.APIUrl+'notes', data);
+  }
+}
