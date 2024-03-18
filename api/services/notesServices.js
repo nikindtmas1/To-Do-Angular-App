ac813e5de8c16c306905f43759b519284f9c405a
Nikolai Nikolaev <niki_nik_74@abv.bg>
2024-03-18 12:10:21 +0200

    Added service update

--- api/services/notesServices.js
+++ api/services/notesServices.js
@@ -4,10 +4,12 @@ const getAllNotes = () => Notes.find();
 const createNotes = (data) => Notes.create(data);
 const getOneNote = (id) => Notes.findById(id);
 const removeNote = (id) => Notes.findByIdAndDelete(id);
+const updateNote = (id, data) => Notes.findByIdAndUpdate(id, data);
 
 module.exports = {
     getAllNotes,
     createNotes,
     getOneNote,
     removeNote,
+    updateNote,
 }
